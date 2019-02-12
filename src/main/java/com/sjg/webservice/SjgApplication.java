dd24fdbef2c6b9cad2ffa7f096df82099c4ce644
burningmagic <burningmagic@nexon.co.kr>
2019-02-12 10:49:01 +0900

    test

---
+++ src/main/java/com/sjg/webservice/SjgApplication.java
@@ -0,0 +1,17 @@
+package com.sjg.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+public class SjgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SjgApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/sjg/webservice/SjgApplicationTests.java
@@ -0,0 +1,17 @@
+package com.sjg.webservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SjgApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+

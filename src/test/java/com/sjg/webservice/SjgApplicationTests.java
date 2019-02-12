6284d85d9168e1fc6cc13a48ed71d4f48a1ae35f
burningmagic <burningmagic@nexon.co.kr>
2019-02-12 15:54:21 +0900

    test2

--- src/test/java/com/sjg/webservice/SjgApplicationTests.java
+++ src/test/java/com/sjg/webservice/SjgApplicationTests.java
@@ -1,16 +1,44 @@
 package com.sjg.webservice;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.List;
+
+import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.sjg.webservice.domain.posts.Posts;
+import com.sjg.webservice.domain.posts.PostsRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SjgApplicationTests {
 
+	@Autowired
+	PostsRepository postsRepository;
+	
+	@After
+	public void cleanup() {
+		postsRepository.deleteAll();
+	}
+	
 	@Test
-	public void contextLoads() {
+	public void 게시글저장_불러오기() {
+		//given
+		postsRepository.save(Posts.builder().title("테스트 게시글").content("테스트 본문").author("Persuader").build());
+		
+		//when
+		List<Posts> postsList = postsRepository.findAll();
+		
+		//then
+		Posts posts = postsList.get(0);
+		assertThat(posts.getTitle(), is("테스트 게시글"));
+		assertThat(posts.getContent(), is("테스트 본문"));
 	}
 
 }
